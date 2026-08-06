5c22c59a8fea9d46f2603cb60499b515b61d3284
Nabakishore Behera <nabakishorebehera57@gmail.com>
2026-08-06 21:46:48 +0530

    Fix dotenv and DNS configuration

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-import dns from "dns";
-import authRoutes from "./routes/authRoutes.js";
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
-dns.setDefaultResultOrder("ipv4first");
-import questionRoute from "./routes/question.js";
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import quizRoutes from "./routes/quizRoutes.js";
 dotenv.config();
 
+import dns from "dns";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+dns.setDefaultResultOrder("ipv4first");
+
+import express from "express";
+import cors from "cors";
+
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import questionRoute from "./routes/question.js";
+import quizRoutes from "./routes/quizRoutes.js";
+
+
 const app = express();
+
 console.log("URI:", process.env.MONGO_URI);
+
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/quiz", quizRoutes);
 app.use("/api/questions", questionRoute);
+
 app.get("/", (req, res) => {
   res.send("🚀 Daily Quiz Backend is Running...");
 });
