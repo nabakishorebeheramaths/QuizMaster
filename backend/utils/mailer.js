47038f19fe6645f4ee4312e263d7e8b234a1f34c
Nabakishore Behera <nabakishorebehera57@gmail.com>
2026-08-06 18:20:33 +0530

    Use Gmail SSL port 465

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -5,15 +5,11 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-    family: 4, // Force IPv4
   },
 });
 
