0b6c3bc8fd54a7fc4c97ced77fd9e403580bcfe7
Nabakishore Behera <nabakishorebehera57@gmail.com>
2026-08-07 01:01:25 +0530

    Fix MongoDB connection error handling

--- config/db.js
+++ config/db.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected Successfully");
-  } catch (error) {
-    console.log("❌ MongoDB Connection Error:");
-    console.log(error.message);
-    process.exit(1);
+
+    console.log("MongoDB Connected Successfully");
+
+  } catch (err) {
+    console.log("❌ MongoDB Connection Error:", err.message);
   }
 };
 
