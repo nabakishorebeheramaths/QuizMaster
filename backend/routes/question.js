9f208f1c97e7edfe0f6e3e890373b8cd7974a17f
Nabakishore Behera <nabakishorebehera57@gmail.com>
2026-08-07 01:21:49 +0530

    Fixed daily quiz same questions system

--- backend/routes/question.js
+++ backend/routes/question.js
@@ -1,34 +1,83 @@
 import express from "express";
 import Question from "../models/Question.js";
+import DailyQuiz from "../models/DailyQuiz.js";
 
 const router = express.Router();
 
-// Get daily random 30 questions
+
+// Get Today's Quiz
 router.get("/", async (req, res) => {
+
   try {
 
-    const questions = await Question.aggregate([
-      {
-        $sample: {
-          size: 30
+    const today = new Date().toISOString().split("T")[0];
+
+
+    // Check today's quiz already exists
+    let dailyQuiz = await DailyQuiz.findOne({
+      date: today
+    }).populate("questions");
+
+
+    // If not exists create new quiz
+    if (!dailyQuiz) {
+
+
+      const randomQuestions = await Question.aggregate([
+        {
+          $sample:{
+            size:30
+          }
         }
-      }
-    ]);
+      ]);
+
+
+      dailyQuiz = await DailyQuiz.create({
+
+        date: today,
+
+        questions: randomQuestions.map(
+          (q)=>q._id
+        )
+
+      });
+
+
+      dailyQuiz = await DailyQuiz.findOne({
+        date: today
+      }).populate("questions");
+
+    }
+
 
     res.status(200).json({
-      success: true,
-      count: questions.length,
-      questions
+
+      success:true,
+
+      date: dailyQuiz.date,
+
+      status:"LIVE",
+
+      count: dailyQuiz.questions.length,
+
+      questions: dailyQuiz.questions
+
     });
 
-  } catch (error) {
+
+  } catch(error){
 
     res.status(500).json({
-      success: false,
-      message: error.message
+
+      success:false,
+
+      message:error.message
+
     });
 
   }
+
 });
+
 
 export default router;
